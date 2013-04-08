af2e9e5b8a57b9c5824e07525b2ea80a6b96b6a9
Cássio Zen <cassio@commit.to>
2013-04-08 12:42:11 -0300

    Added phospr animation for giraboorger

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,4 @@
 $(function(){
-    // Work carousel
-    $('.work').scrollable({circular: true, touch: false}).autoscroll({interval: 10000}).navigator('.workNavi')
-
 
 	//Check for canvas support
 	if (!document.createElement('canvas').getContext) return;
@@ -112,4 +109,112 @@ $(function(){
         $("html").css({background: bgColors[idx]});
 	})
 	image = image[0]
+
+  //
+  // Phospr animation for Giraboorger
+  //
+
+  player_giraboorguer = new PhosphorPlayer('anim_target_giraboorguer');
+  phosphorCallback_giraboorguer = function(data) {
+
+    /**
+     * Instantiate the player.  The player supports a variate of callbacks for deeper integration into your site.
+     */
+
+     giraboorguer_framecount = data.frames.length;
+     player_giraboorguer.load_animation({
+      imageArray:["giraboorguer_atlas000.jpg"],
+      imagePath: "/images/work/",
+      animationData: data,
+      loop: true,
+      onLoad: function() {
+        player_giraboorguer.play();
+
+        /**
+         * If your Phosphor composition was created with the "interactive" mode set, the code below enables that
+         * interation.  Handlers are registered for both mouse drag and touch events.
+         */
+
+         var trappedMouse = false;
+         var trappedXPos;
+
+         var enableInteractivity = false;
+
+         if(enableInteractivity) {
+          $("#anim_target_giraboorguer").mousedown(function(e){
+            e.preventDefault();
+            player_giraboorguer.stop();
+            trappedMouse = true;
+            trappedXPos = e.pageX;
+            $(document).bind('mousemove',function(event) {
+              if(trappedMouse){
+                var pos =  (event.pageX - trappedXPos) / 5;
+                var seekTime = (giraboorguer_framecount + player_giraboorguer.currentFrameNumber() + parseInt(pos)) % giraboorguer_framecount;
+                player_giraboorguer.setCurrentFrameNumber(seekTime);
+                trappedXPos = event.pageX;
+              }
+
+            });
+
+          });
+
+          $(document).mouseup(function(e){
+            trappedMouse = false;
+            $(document).unbind('mousemove');
+          });
+
+        
+
+          $("#anim_target_giraboorguer").bind("touchstart",function(event){
+           var e = event.originalEvent;
+           e.preventDefault();
+           player_giraboorguer.stop();
+           trappedMouse = true;
+           trappedXPos = e.pageX;
+           $(document).bind('touchmove', function(e) {
+            if(trappedMouse){
+              var e = e.originalEvent;
+              e.preventDefault();
+              var pos =  (e.pageX - trappedXPos) / 5;
+              var seekTime = (giraboorguer_framecount + player_giraboorguer.currentFrameNumber() + parseInt(pos)) % giraboorguer_framecount;
+              player_giraboorguer.setCurrentFrameNumber(seekTime);
+              trappedXPos = e.pageX;
+            }
+           });
+         });
+
+          $("#anim_target_giraboorguer").bind("touchend",function(event){
+           var e = event.originalEvent;
+           e.preventDefault();
+           trappedMouse = false;
+           player_giraboorguer.play(true);
+           $(document).unbind('touchmove');
+         });
+
+        }
+
+      }
+    });
+   }
+   var jsonpScript = document.createElement("script");
+   jsonpScript.type = "text/javascript";
+   jsonpScript.id = "jsonPinclude_giraboorguer";
+   jsonpScript.src = "javascripts/giraboorguer_animationData.jsonp";
+   document.getElementsByTagName("head")[0].appendChild(jsonpScript);
+
+  //
+  // Work carousel
+  //
+  $('.work').scrollable({circular: true, touch: false, onSeek:carouselSeek }).autoscroll({interval: 10000}).navigator('.workNavi');
+
+  // Plays/pauses the phospr on the first index
+  function carouselSeek(event){
+    if(event.target.getIndex() != 0) {
+      player_giraboorguer.stop()
+    } else {
+      player_giraboorguer.play()
+    }
+  }
+
+
 })
